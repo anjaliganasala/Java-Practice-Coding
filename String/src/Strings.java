d62817c506c50c4a66d83d97712e601c89096692
anjaliganasala <160149065+anjaliganasala@users.noreply.github.com>
2025-05-04 17:17:09 +0530

    Day10

---
+++ String/src/Strings.java
@@ -0,0 +1,10 @@
+
+public class Strings {
+public static void main(String[] args) {
+	String s1 = new String("flm");
+	String s2 = new String("flm");
+	
+	System.out.println(s1.equals(s2));
+	
+}
+}
