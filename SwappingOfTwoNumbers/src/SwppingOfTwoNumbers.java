b27901c2ce6337dd1de6a3258daa81c2de78340a
anjaliganasala <160149065+anjaliganasala@users.noreply.github.com>
2025-05-04 17:26:38 +0530

    Add files via upload

---
+++ SwappingOfTwoNumbers/src/SwppingOfTwoNumbers.java
@@ -0,0 +1,15 @@
+
+public class SwppingOfTwoNumbers {
+public static void main(String[] args) {
+	int a = 10;
+	int b = 20;
+	System.err.println("before swapping of two numbers:");
+	System.out.println("a ="+a+",b ="+b);
+	int temp = a;
+	 a = b;
+	 b = temp;
+	 System.out.println("After swapping of two numbers :");
+	 System.out.println("a ="+a+",b ="+b);
+	 
+}
+}
