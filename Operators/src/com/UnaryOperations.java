40610bc894defb84f5ed742e69435ab2c20a95e6
anjaliganasala <160149065+anjaliganasala@users.noreply.github.com>
2025-05-04 11:26:45 +0530

    Day2

---
+++ Operators/src/com/UnaryOperations.java
@@ -0,0 +1,19 @@
+package com;
+
+public class UnaryOperations {
+public static void main(String[] args) {
+	
+	
+	
+	
+	int a = 1;
+	a++;
+	System.out.println(a);
+	System.out.println(a++);
+	System.out.println(a++);
+	System.out.println(a);
+	
+	
+	
+}
+}
