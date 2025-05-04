472a9c384a5bc2228c1af37b85affed4c6ba2404
anjaliganasala <160149065+anjaliganasala@users.noreply.github.com>
2025-05-04 17:22:38 +0530

    Add files via upload

---
+++ ChatAt/src/com/ChatAt.java
@@ -0,0 +1,12 @@
+package com;
+
+public class ChatAt {
+	public static void main(String[] args) {
+		String name = "Anjali";
+		for(int i=0 ; i<=name.length(); i++) {
+			System.out.println(name.charAt(i));
+
+		}
+	}
+
+}
