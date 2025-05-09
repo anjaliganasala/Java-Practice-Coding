c8171d8ae0120e26e891ce2c0a6a064a8dc4b611
anjaliganasala <160149065+anjaliganasala@users.noreply.github.com>
2025-05-09 08:04:27 +0530

    Add files via upload

---
+++ Strings/src/String.java
@@ -0,0 +1,10 @@
+
+public class String {
+public static void main(String[] args) {
+	String obj = new String();
+    obj.findLength("Anjali");
+}
+ static void findLength(String s) {
+	 System.out.println(s.Length());
+ }
+}
