dc8035bcba353cec7040e2b41a680954cb21db0c
anjaliganasala <160149065+anjaliganasala@users.noreply.github.com>
2025-05-14 22:37:12 +0530

    Add files via upload

---
+++ Oops_Single_Inheritance/src/com/ChildAnimal.java
@@ -0,0 +1,11 @@
+package com;
+
+public class ChildAnimal extends ParentAnimal{
+
+	String s3 = "child dog1";
+	String s4 = "child dog2";
+
+	void barkingChild() {
+		System.out.println("child dogs....");
+	}
+}
---
+++ Oops_Single_Inheritance/src/com/ParentAnimal.java
@@ -0,0 +1,11 @@
+package com;
+
+public class ParentAnimal {
+
+	String s1 = "parent dog1";
+	String s2 = "parent dog2";
+	 
+	 void barkingParent() {
+		 System.out.println("parent dogs...");
+	 }
+}
