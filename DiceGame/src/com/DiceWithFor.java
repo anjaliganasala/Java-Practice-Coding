620ce3d07c1ad09dc73c0251c0c838d7ff4db36e
anjaliganasala <160149065+anjaliganasala@users.noreply.github.com>
2025-05-30 16:39:01 +0530

    Add files via upload

---
+++ DiceGame/src/com/DiceWithFor.java
@@ -0,0 +1,36 @@
+package com;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DiceWithFor {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random random = new Random();
+
+		int score = 0;
+		int input;
+
+		System.out.println("starting score = 0");
+		for (int i = 0; i < 5; i++) {         // only 6 chances 0 to 5
+			System.out.print("Enter 10 to roll the dice:");
+			input = sc.nextInt();
+
+			if (input != 10) {
+				System.out.print("Invalid! try again");
+				continue;
+			}
+			int dice = random.nextInt(6)+1;          // dice values ( 1 - 6 only)
+			System.out.println("Dice value :" + dice);
+
+			if (dice == 1) {
+				System.out.println("you are out of the game");
+				break;
+			} else {
+				score += dice;
+			}
+		}
+		System.out.println("Final score :" + score);
+}
+}
